80fccf96623a60c2b3854a2255926ba69cf155b6
victor <victor@anon.lan>
2015-11-24 20:13:29 +0100

    reorganized

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
-	/*When the .menu-trigger is clicked*/	
-	
+	/*When the .menu-trigger is clicked*/
+
     $(".menu-trigger").click(function() {
 		/*make the .menu visible, with an animation that lasts 400 milliseconds */
 		$(".menu").slideToggle(400, function() {
-            
+
             $(this).toggleClass("nav-expanded").css('display', '');
-            
+
         });
-			
-		
+
+
 	});
-	
+
 });
-
-
